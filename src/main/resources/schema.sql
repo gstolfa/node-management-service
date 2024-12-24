c6bbbc9b094e8b275e90ffffaffff030b185a658
Gianfranco Stolfa <gianfranco.stolfa@gmail.com>
2024-12-24 11:20:27 +0100

    Update schema.sql

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,20 +1,20 @@
--- Crea la tabella `nodes` se non esiste
+
 CREATE TABLE IF NOT EXISTS nodes (
-                                     id BIGINT AUTO_INCREMENT PRIMARY KEY,        -- Identificativo unico per il nodo
-                                     name VARCHAR(255) NOT NULL UNIQUE            -- Nome del nodo, deve essere unico
+                                     id BIGINT AUTO_INCREMENT PRIMARY KEY,
+                                     name VARCHAR(255) NOT NULL UNIQUE
     );
 
--- Crea la tabella `node_relationships` se non esiste
+
 CREATE TABLE IF NOT EXISTS node_relationships (
-                                                  id BIGINT AUTO_INCREMENT PRIMARY KEY,        -- Identificativo unico per la relazione
-                                                  ancestor_id BIGINT NOT NULL,                 -- ID del nodo ancestro
-                                                  descendant_id BIGINT NOT NULL,               -- ID del nodo discendente
-                                                  depth INT NOT NULL,                          -- Profondità della relazione
-                                                  FOREIGN KEY (ancestor_id) REFERENCES nodes(id) ON DELETE CASCADE, -- Relazione con il nodo ancestro
+                                                  id BIGINT AUTO_INCREMENT PRIMARY KEY,
+                                                  ancestor_id BIGINT NOT NULL,
+                                                  descendant_id BIGINT NOT NULL,
+                                                  depth INT NOT NULL,
+                                                  FOREIGN KEY (ancestor_id) REFERENCES nodes(id) ON DELETE CASCADE,
     FOREIGN KEY (descendant_id) REFERENCES nodes(id) ON DELETE CASCADE -- Relazione con il nodo discendente
     );
 
--- Se il nodo 'root' non esiste, inseriscilo nella tabella `nodes`
+
 INSERT INTO nodes (id, name)
 SELECT 1, 'root'
     WHERE NOT EXISTS (SELECT 1 FROM nodes WHERE name = 'root');
